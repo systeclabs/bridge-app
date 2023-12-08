83f10b04ea9d36b17b1642cbbb8366b81a808697
Ivan Martinez <ivanmartinezaguero@gmail.com>
2023-12-07 20:53:19 -0700

    :lipstick: integrates card component

---
+++ frontend/src/app/components/molecules/Card.tsx
@@ -0,0 +1,11 @@
+const Card = () => {
+    return (
+        <div className="card">
+            <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+            </div>
+        </div>
+    )
+}
+
+export default Card
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,5 +1,9 @@
+import Card from './components/molecules/Card'
+
 export default function Home() {
   return (
-    <></>
+    <>
+      <Card />
+    </>
   )
 }
