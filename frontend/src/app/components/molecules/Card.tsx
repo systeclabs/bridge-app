2573e90d767c1544bcfa55de40e20567da61001e
Ivan Martinez <ivanmartinezaguero@gmail.com>
2023-12-07 21:58:13 -0700

    :lipstick: adds types to card component

--- frontend/src/app/components/molecules/Card.tsx
+++ frontend/src/app/components/molecules/Card.tsx
@@ -1,8 +1,18 @@
-const Card = () => {
+import React from 'react'
+
+interface TasksProps {
+    task: {
+        id: number;
+        title: string;
+    }
+}
+
+const Card: React.FC<TasksProps> = ({ task }) => {
     return (
         <div className="card">
             <div className="card-body">
-                <h5 className="card-title">Card title</h5>
+                <h5 className="card-title">{ task.title }</h5>
+                <p>{ task.id }</p>
             </div>
         </div>
     )
